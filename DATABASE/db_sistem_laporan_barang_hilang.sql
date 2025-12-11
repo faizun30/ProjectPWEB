b0217329702cfa6eb6dd60c21b8671e39a653a8f
ProjectPWEB <alamandafaiz@gmail.com>
2025-12-11 15:29:48 +0700

    Penambahn database

--- DATABASE/db_sistem_laporan_barang_hilang.sql
+++ DATABASE/db_sistem_laporan_barang_hilang.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 11 Des 2025 pada 09.20
+-- Waktu pembuatan: 11 Des 2025 pada 09.28
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -42,7 +42,12 @@ CREATE TABLE `barang_temuan` (
 
 INSERT INTO `barang_temuan` (`id`, `nama_barang`, `lokasi_ditemukan`, `tanggal_ditemukan`, `ciri_ciri`, `status`) VALUES
 (1, 'Dompet', 'Gedung 3 Lantai 1', '2025-12-10', 'Warna coklat , bahan kulit', 'Sudah Diambil'),
-(3, 'Jam Tangan', 'kampus H Gedung 4 Lantai 2', '2025-12-05', 'Warna Emas metalic , bahan besi metal', 'Belum Diambil');
+(3, 'Jam Tangan', 'kampus H Gedung 4 Lantai 2', '2025-12-05', 'Warna Emas metalic , bahan besi metal', 'Belum Diambil'),
+(5, 'Cincin', 'Parkiran Kampus E', '2025-12-16', 'Cincin berbahan emas seberat 10kg', 'Belum Diambil'),
+(6, 'Uang Coper', 'Parkiran Kampus D', '2025-12-10', 'Coper yang berisi uang 1 T', 'Sudah Diambil'),
+(7, 'Earpods', 'Kampus H Gedung 3 Lantai 1', '2025-12-09', 'Berwarna putih merk larbogini', 'Belum Diambil'),
+(8, 'HandPhone', 'Parkiran Kampus D Gedung 321', '2025-12-12', 'HP Merk Vertu terbaru', 'Sudah Diambil'),
+(9, 'Parfum ', 'Parkiran Kampus D', '2025-12-05', 'Parfum merk diorsage senilai 4 jt', 'Belum Diambil');
 
 --
 -- Indexes for dumped tables
@@ -62,7 +67,7 @@ ALTER TABLE `barang_temuan`
 -- AUTO_INCREMENT untuk tabel `barang_temuan`
 --
 ALTER TABLE `barang_temuan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
