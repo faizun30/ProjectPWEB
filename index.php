e6aa5fe0e3a057eeaf2bd2715ddf9bac2ea32837
ProjectPWEB <alamandafaiz@gmail.com>
2025-12-12 22:47:14 +0700

    Perbaikan Code

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard Lost & Found</title>
+    <title>Dashboard Sistem Laporan Barang Hilang</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
