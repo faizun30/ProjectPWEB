f5ed209e8c258ecccb9f64d9535bfa4518cf8639
Faiz Alamanda Zaky <alamandafaiz@gmail.com>
2026-01-10 09:17:41 +0700

    Update hapus.php

--- hapus.php
+++ hapus.php
@@ -1,7 +1,6 @@
 <?php
 include 'koneksi.php';
 
-// Tangkap ID dari URL
 $id = $_GET['id'];
 
 // Query Hapus
@@ -9,7 +8,7 @@ $query = "DELETE FROM barang_temuan WHERE id='$id'";
 $result = mysqli_query($koneksi, $query);
 
 if ($result) {
-    // Jika berhasil hapus, langsung balik ke index
+    
     header("Location: index.php");
 } else {
     echo "Gagal menghapus data.";
